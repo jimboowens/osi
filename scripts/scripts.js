99aa98f05d8dc128ed79b634bae754ee0d4fbd13
Jeremy Owens <jimcowens@gmail.com>
2019-08-19 12:25:57 -0400

    joel update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,14 +2,13 @@ let main = document.querySelector('.main')
 let windowHTML = document.querySelector('html')
 let mainBODY = document.querySelector("body")
 
-let mainHTML=`
+let navBarHTML=` 
     <nav class="z-depth-5 navBar">
         <ul>
             <li><a class="OSIJobs waves-effect waves-light navOptions" href="#splash">OSI Jobs</a></li>
             <li><a class="social facebook" href="http://www.facebook.com/pages/OSI/314041371978411" target="blank">f</a></li>
             <li><a class="social linkedIn" href="http://www.linkedin.com/company/2497623" target="blank">i</a></li>
             <li><a class="social twitter" href="http://twitter.com/osifoodjobs" target="blank">l</a></li>
-            
         </ul>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
             <li><a class="waves-effect waves-light navOptions" href="#about">About</a></li>
@@ -29,6 +28,9 @@ let mainHTML=`
             <a class="waves-effect waves-light dropdownOptions" href="#contact">Contact</a>
         </div>
     </nav>
+`
+
+let mainHTML= `   
     <div class="parallax">
         <div id="splash" class="parallax_group">
             <div class="parallax_layer parallax_layer--base z-depth-5">
@@ -43,18 +45,34 @@ let mainHTML=`
         </div>
         <div id="about" class="parallax_group">
             <div class="parallax_layer parallax_layer--base">
-                <div class="title">Base Layer 2</div>
+                <div class="title aboutBaseTitle imageClass">
+                    <img class="aboutImage z-depth-5" src="./public/images/licensed/meals/zucchini_into_plate.JPG" />
+                    <img class="aboutImage z-depth-5" src="./public/images/licensed/meals/zucchini_into_plate.JPG" />
+                    <img class="aboutImage z-depth-5" src="./public/images/licensed/meals/zucchini_into_plate.JPG" />
+                    <img class="aboutImage z-depth-5" src="./public/images/licensed/meals/zucchini_into_plate.JPG" />
+                    <img class="aboutImage z-depth-5" src="./public/images/licensed/meals/zucchini_into_plate.JPG" />
+                    <img class="aboutImage z-depth-5" src="./public/images/licensed/meals/zucchini_into_plate.JPG" />
+                    <img class="aboutImage z-depth-5" src="./public/images/licensed/meals/zucchini_into_plate.JPG" />
+                    <img class="aboutImage z-depth-5" src="./public/images/licensed/meals/zucchini_into_plate.JPG" />
+                    <img class="aboutImage z-depth-5" src="./public/images/licensed/meals/zucchini_into_plate.JPG" />
+                    <img class="aboutImage z-depth-5" src="./public/images/licensed/meals/zucchini_into_plate.JPG" />
+                    <img class="aboutImage z-depth-5" src="./public/images/licensed/meals/zucchini_into_plate.JPG" />
+                    <img class="aboutImage z-depth-5" src="./public/images/licensed/meals/zucchini_into_plate.JPG" />
+                    <img class="aboutImage z-depth-5" src="./public/images/licensed/meals/zucchini_into_plate.JPG" />
+                    <img class="aboutImage z-depth-5" src="./public/images/licensed/meals/zucchini_into_plate.JPG" />
+                    <img class="aboutImage z-depth-5" src="./public/images/licensed/meals/zucchini_into_plate.JPG" />
+                </div>
             </div>
             <div class="parallax_layer parallax_layer--back">
-                <div class="title">Background Layer</div>
+                <div class="title aboutBackgroundTitle">Background Layer</div>
             </div>
         </div>
         <div id="jobs" class="parallax_group darkText">
             <div class="parallax_layer parallax_layer--fore">
-                <div class="title">Foreground Layer 1</div>
+                <div class="title jobsForegroundTitle">Foreground Layer 1</div>
             </div>
             <div class="parallax_layer parallax_layer--base">
-                <div class="title">Base Layer 3</div>
+                <div class="title jobsBaseTitle">Base Layer 3</div>
             </div>
         </div>
         <div id="recruiting" class="parallax_group">
@@ -94,20 +112,37 @@ let mainHTML=`
         </div>
     </div>
 `
-
+      
 $(main).html(mainHTML)
 
-$(document).ready(()=>{
-    
-    $("*").css({
-        "margin":"0",
-    })
+setTimeout(() => {
+    $(".ball").css({
+        "position":"unset",
+        "visibility":"none !important",
+        "border":"none",
+        "margin-top":"43px",
+        "animation":"none"
+    });
+}, 2000);
 
-    $(".navOptions").hover(
-        function(){
-            $(this).css("transform","scale(1.2)")
-        },function(){
-            $(this).css("transform","scale(1)")
-        }
-    )
+setTimeout(() => {
+        $(main).prepend(navBarHTML);
+        $(".navOptions").hover(
+            function(){
+                $(this).css({
+                    "transform":"scale(1.2)",
+                    "color": "#88D498"
+                })
+            },function(){
+                $(this).css({
+                    "transform":"scale(1)",
+                    "color": "F3E9D2"
+                })
+            }
+        )
+    }, 2985);
+
+$(document).ready(()=>{
+  
+    
 })
